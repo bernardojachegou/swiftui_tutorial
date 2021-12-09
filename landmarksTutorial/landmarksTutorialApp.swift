089ed775dd48cb44166621782233a2d320d1c276
Michel Bernardo <michel.bernardo@db1.com.br>
2021-12-09 13:58:35 -0400

    Initial Commit

---
+++ landmarksTutorial/landmarksTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksTutorialApp.swift
+//  landmarksTutorial
+//
+//  Created by Michel Franklin Silva Bernardo on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct landmarksTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
