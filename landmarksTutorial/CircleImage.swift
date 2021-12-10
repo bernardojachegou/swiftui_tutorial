37d41c8b4526d41229c730430974ad39cd5f193d
Michel Bernardo <michel.bernardo@db1.com.br>
2021-12-09 21:36:12 -0400

    uploading free apple swiftui tutorial

---
+++ landmarksTutorial/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarksTutorial
+//
+//  Created by Michel Franklin Silva Bernardo on 09/12/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
