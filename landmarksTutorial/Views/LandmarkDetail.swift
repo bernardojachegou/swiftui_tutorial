a3e001afa7e35ab498b172e479c3023e0f455721
Michel Bernardo <michel.bernardo@db1.com.br>
2022-01-04 14:17:41 -0300

    adding links to handle the landmarks details

---
+++ landmarksTutorial/Views/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  landmarksTutorial
+//
+//  Created by Michel Franklin Silva Bernardo on 04/01/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
