a64b9d1234f1647d5e2ca4b6e23e82f5b613fa01
Michel Bernardo <michel.bernardo@db1.com.br>
2022-01-13 14:21:58 -0300

    handling the macOS app

--- landmarksTutorial/Views/Landmarks/LandmarkRow.swift
+++ landmarksTutorial/Views/Landmarks/LandmarkRow.swift
@@ -9,27 +9,38 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
-            
+                .cornerRadius(5)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .bold()
+                #if !os(watchOS)
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                #endif
+            }
+
             Spacer()
-            
+
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .imageScale(.medium)
                     .foregroundColor(.yellow)
             }
         }
+        .padding(.vertical, 4)
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-    
+
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
